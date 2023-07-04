ce373ed0efd4a4eeeb5220e40a67b3d0cf4ddc6e
Abd al Rahman Gad <89566409+AbdAlRahmanGad@users.noreply.github.com>
2023-07-04 07:33:01 +0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -6,53 +6,61 @@ import java.util.ArrayList;
 
 public class Game implements ActionListener {
     JFrame frame;
+    int WIDTH = 500;
+    int HEIGHT = 500;
 
-    JLabel ship;
-    JLabel enemy;
     JLabel scoreLabel;
     JLabel livesLabel;
+    int score =0;
+    int lives =3;
+    int start = 0;
 
-    Action upAction;
-    Action downAction;
-    Action leftAction;
-    Action rightAction;
+
+
+
+
+
+
 
     int enemyBulletNumber=0;
     int enemyBulletNumber1=0;
-
-    int bulletNumber1 =0;
-    int score =0;
-    int lives =3;
-    int movement= 15;
-    int WIDTH = 500;
-    int HEIGHT = 500;
-    int direction = 1;
     int bulletNumber = 0;
-    int start = 0;
-    ArrayList<JLabel> enemies = new ArrayList<>();
+    int bulletNumber1 =0;
+
+
+    int direction = 1;
     Timer moveAlien;
     Timer moveBullet;
     Timer setBullet;
-    Timer  moveEnemyBullet;
+    Timer moveEnemyBullet;
     Timer setEnemyBullet;
-    int moveShipBulletSpeed;
-    int moveShipBulletTimer;
-    int moveEnemyBulletSpeed;
-    int moveEnemyBulletTimer;
+
+    ArrayList<Alien> aliens = new ArrayList<>();
+    SpaceShip ship = new SpaceShip(HEIGHT,WIDTH);
+
     Game(int myBulletSpeed , int enemyBulletSpeed, int screenMode,int bulletMode){
+        /// place aliens
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                Alien alien  =new Alien();
+                alien.getAlienLabel().setBounds(i*40, j*40, 30, 30);
+                aliens.add(alien);
+            }
+        }
+
         if(myBulletSpeed == 1 ){
-            moveShipBulletSpeed = 25;
-                    moveShipBulletTimer = 2000;
+             ship.setMoveShipBulletSpeed(25);
+             ship.setMoveShipBulletTimer(2000);
         }else{
-            moveShipBulletSpeed = 50;
-            moveShipBulletTimer = 3000;
+            ship.setMoveShipBulletSpeed(50);
+            ship.setMoveShipBulletTimer(3000);
         }
         if(enemyBulletSpeed == 1 ){
-            moveEnemyBulletSpeed = 25;
-                    moveEnemyBulletTimer = 2000;
+            Alien.setMoveAlienBulletSpeed(25);
+            Alien.setMoveAlienBulletTimer(2000);
         }else{
-            moveEnemyBulletSpeed = 50;
-            moveEnemyBulletTimer = 3000;
+            Alien.setMoveAlienBulletSpeed(50);
+            Alien.setMoveAlienBulletTimer(3000);
         }
         /// score Label
         scoreLabel = new JLabel("Score : "+score);
@@ -60,26 +68,6 @@ public class Game implements ActionListener {
         /// score Label
         livesLabel = new JLabel("lives left : "+lives);
         livesLabel.setBounds(0,350,100,30);
-
-
-        /// spaceship icon
-        ImageIcon spaceShip = new ImageIcon("src/spaceship.png");
-        Image img = spaceShip.getImage();
-        Image newimg = img.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
-        /// aliens icon
-        ImageIcon alien = new ImageIcon("src/alliens.png");
-        Image img2 = alien.getImage();
-        Image alien2 = img2.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
-        alien.setImage(alien2);
-        /// place aliens
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 5; j++) {
-                enemy = new JLabel(alien);
-                enemy.setBounds(i*40, j*40, 30, 30);
-                enemies.add(enemy);
-
-            }
-        }
 
         /// moving aliens timer
         moveAlien = new Timer(50,this);
@@ -102,24 +90,7 @@ public class Game implements ActionListener {
             enemyBullets.add(bulletEnem);
         }
         ///set spaceShip
-        spaceShip.setImage(newimg);
-        ship = new JLabel(spaceShip);
-        ship.setBounds(WIDTH-(WIDTH/2), HEIGHT-75, 30, 30);
-        /// key bindings
-        upAction = new UpAction();
-        downAction = new DownAction();
-        leftAction = new LeftAction();
-        rightAction = new RightAction();
-        ship.getInputMap().put(KeyStroke.getKeyStroke('w'), "upAction");
-        ship.getInputMap().put(KeyStroke.getKeyStroke('s'), "downAction");
-        ship.getInputMap().put(KeyStroke.getKeyStroke('a'), "leftAction");
-        ship.getInputMap().put(KeyStroke.getKeyStroke('d'), "rightAction");
-        ship.getActionMap().put("upAction", upAction);
-        ship.getActionMap().put("leftAction", leftAction);
-        ship.getActionMap().put("rightAction", rightAction);
-        ship.getActionMap().put("downAction", downAction);
-
-
+//        ship.setBounds(WIDTH-(WIDTH/2), HEIGHT-75, 30, 30);
         /// Frame stuff
         frame = new JFrame("Space Invaders");
         if(screenMode == 1){
@@ -133,10 +104,10 @@ public class Game implements ActionListener {
         }
         frame.setBackground(Color.gray);
 
-        frame.add(ship);
+        frame.add(ship.getShip());
         frame.add(bullet);
-        for (int i = 0; i < enemies.size(); i++) {
-            frame.add(enemies.get(i));
+        for (int i = 0; i < aliens.size(); i++) {
+            frame.add(aliens.get(i).getAlienLabel());
         }
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -146,14 +117,14 @@ public class Game implements ActionListener {
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
 
-         setEnemyBullet = new Timer(moveEnemyBulletTimer,new AbstractAction("bullet begin enemy") {
+         setEnemyBullet = new Timer(Alien.getMoveAlienBulletTimer(),new AbstractAction("bullet begin enemy") {
             @Override
             public void actionPerformed( ActionEvent e ) {
                 JLabel b = enemyBullets.get(enemyBulletNumber);
 
-                int randomNum = (int) (Math.random() * ((enemies.size())));
+                int randomNum = (int) (Math.random() * ((aliens.size())));
 
-                JLabel enemyBullet = enemies.get(randomNum);
+                JLabel enemyBullet = aliens.get(randomNum).getAlienLabel();
                 b.setBounds(enemyBullet.getX(),enemyBullet.getY(),50,50);
                 enemyBulletNumber1 = enemyBulletNumber;
                 frame.add(b);
@@ -161,7 +132,7 @@ public class Game implements ActionListener {
             }
         });
         setEnemyBullet.start();
-        moveEnemyBullet = new Timer(moveEnemyBulletSpeed,new AbstractAction("enemy bullet move") {
+        moveEnemyBullet = new Timer(Alien.getMoveAlienBulletSpeed(),new AbstractAction("enemy bullet move") {
             @Override
             public void actionPerformed( ActionEvent e ) {
                 JLabel b = enemyBullets.get(enemyBulletNumber1);
@@ -177,8 +148,8 @@ public class Game implements ActionListener {
                         }
                     }
                 }
-                if(b.getY()>= ship.getY()-20&& b.getY()<= ship.getY()){
-                    if(b.getX()>= ship.getX()-27&& b.getX()<= ship.getX()+5){
+                if(b.getY()>= ship.getShip().getY()-20&& b.getY()<= ship.getShip().getY()){
+                    if(b.getX()>= ship.getShip().getX()-27&& b.getX()<= ship.getShip().getX()+5){
 //                            frame.remove(shipBullet);
 //                        ship.setLocation(ship.getX()+10, ship.getY()+10);
                         b.setLocation(-50,-50);
@@ -196,18 +167,18 @@ public class Game implements ActionListener {
             }
         });
         moveEnemyBullet.start();
-         setBullet = new Timer(moveShipBulletTimer,new AbstractAction("bullet begin") {
+         setBullet = new Timer(ship.getMoveShipBulletTimer(),new AbstractAction("bullet begin") {
             @Override
             public void actionPerformed( ActionEvent e ) {
                 JLabel b = bullets.get(bulletNumber);
-                b.setBounds(ship.getX()-10,ship.getY()-15,50,50);
+                b.setBounds(ship.getShip().getX()-10,ship.getShip().getY()-15,50,50);
                 bulletNumber1 = bulletNumber;
                 frame.add(b);
                 bulletNumber = (bulletNumber+1)%3;
             }
         });
         setBullet.start();
-         moveBullet = new Timer(moveShipBulletSpeed,new AbstractAction("bullet move") {
+         moveBullet = new Timer(ship.getMoveShipBulletSpeed(),new AbstractAction("bullet move") {
             @Override
             public void actionPerformed( ActionEvent e ) {
                 JLabel b = bullets.get(bulletNumber1);
@@ -215,12 +186,12 @@ public class Game implements ActionListener {
                 if(start <= 50){
                     return;
                 }
-                for (int i = 0; i <enemies.size() ; i++) {
-                    JLabel enemy = enemies.get(i);
+                for (int i = 0; i <aliens.size() ; i++) {
+                    JLabel enemy = aliens.get(i).getAlienLabel();
                     if(b.getY()>= enemy.getY()&& b.getY()<= enemy.getY()+20){
                         if(b.getX()+20>= enemy.getX()-10&& b.getX()+20<= enemy.getX()+30){
                                 frame.remove(enemy);
-                                enemies.remove(i);
+                                aliens.remove(i);
                                 //  super bullet
                             if(bulletMode == 1){
                                 b.setLocation(-50, -50);
@@ -243,7 +214,7 @@ public class Game implements ActionListener {
         frame.add(scoreLabel);
     }
     public void setScore(){
-        int enemiesLeft = enemies.size();
+        int enemiesLeft = aliens.size();
         int newScore =  20 * (50-enemiesLeft);
         scoreLabel.setText("Score : "+newScore );
         score = newScore;
@@ -264,41 +235,13 @@ public class Game implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        if(enemies.get(enemies.size()-1).getX()+30 >= 500 || enemies.get(0).getX() <= 0){
+        if(aliens.get(aliens.size()-1).getAlienLabel().getX()+30 >= 500 || aliens.get(0).getAlienLabel().getX() <= 0){
             direction *=-1;
         }
-        for (int i = 0; i < enemies.size(); i++) {
-            JLabel enemy = enemies.get(i);
+        for (int i = 0; i < aliens.size(); i++) {
+            JLabel enemy = aliens.get(i).getAlienLabel();
             enemy.setLocation(enemy.getX()-direction,enemy.getY());
         }
     }
 
-    public class UpAction extends AbstractAction   {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-                    if(ship.getY() > HEIGHT-100)
-                 ship.setLocation(ship.getX(), ship.getY()-movement);
-        }
-    }
-    public class DownAction extends AbstractAction{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(ship.getY() <HEIGHT-70 )
-                ship.setLocation(ship.getX(), ship.getY() + movement);
-        }
-    }
-    public class LeftAction extends AbstractAction{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(ship.getX() > 10 )
-                ship.setLocation(ship.getX() - movement, ship.getY());
-        }
-    }
-    public class RightAction extends AbstractAction{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(ship.getX() < WIDTH-40 )
-                ship.setLocation(ship.getX()+movement, ship.getY());
-        }
-    }
 }
